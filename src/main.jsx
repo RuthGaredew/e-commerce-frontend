a9e9da92a29611168617fb4fc8fd28ec395ad203
Ruth Garedew <ruthgaredew@gmail.com>
2026-04-08 15:39:31 +0300

    Fixing all file casing and clearing git cache

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { EcommerceProvider } from "./context/EcommerceContext";
-import { UserProvider } from "./context/UserContext"; 
+import { UserProvider } from "./context/UserContext";
 import App from "./App";
-import "C:/Users/Kelela/Documents/GitHub/e-commerce-frontend/src/styles/index.css"; 
+import "C:/Users/Kelela/Documents/GitHub/e-commerce-frontend/src/styles/index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <UserProvider>
     <EcommerceProvider>
